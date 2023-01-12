eb6456e6170d7c40ed21b312490dbddf4a10bd81
Joel Buchheim-Moore <joel@thing.com>
2023-01-12 11:51:32 -0800

    updated some docs

--- src/options/registration.rs
+++ src/options/registration.rs
@@ -14,19 +14,22 @@ pub struct RegistrationOptionItem(Option<WampDict>);
 
 /// Provides functions for adding defined options to the WampDict
 impl RegistrationOptionItem {
-    /// Add an option for pattern matching the topic of the subscription
+    /// Add an option for how the router should invoke the endpoint
     pub fn invoke(invoke_option: InvokeOption) -> Self {
         RegistrationOptions::new().with_invoke(invoke_option)
     }
 
+    /// Add an option for how the router should invoke the endpoint
     pub fn with_invoke(&self, invoke_option: InvokeOption) -> Self {
         self.with_option(WampOption::RegisterOption("invoke".to_owned(), Arg::String(invoke_option.value())))
     }
 
+    /// Add an option for how the router should match the rpc.
     pub fn match_(match_option: MatchOption) -> Self {
         RegistrationOptions::new().with_match(match_option)
     }
 
+    /// Add an option for how the router should match the rpc.
     pub fn with_match(&self, match_option: MatchOption) -> Self {
         self.with_option(WampOption::RegisterOption("match".to_owned(), Arg::String(match_option.value())))
     }
